9d46d086c1a81559d28ddd796bb5aca50f00f2bf
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-05-28 19:43:11 -0600

    Clase 28 mayo: endpoints, status code y metodos HTTP

--- Semana 6/backend/server.js
+++ Semana 6/backend/server.js
@@ -9,3 +9,40 @@ app.listen(port,()=>{
 } );
 
 console.log("Esta linea 'deberia' ejecutarse despues del listen", port);
+
+
+/*
+GET -> READ 
+POST -> CREATE
+PUT -> UPDATE 
+DELETE -> DELETE
+*/
+
+/*
+1) BE tiene el control TOTAL.
+  a) Establece como vamos a realizar las peticiones
+  b) BE decide a quien o a que responder 
+  c) BE decide QUE va a responder 
+  d) BE decide como gestiona la información que se le envia
+  e) BE decide que es un error / o que se considera un error
+*/
+
+/*
+    Terminos importantes: 
+    - Ruta: una dirección a la cual dirigiremos la solicitud
+    - Payload: información que viene con la solicitud.
+    - Endpoint: es la combinación de la ruta + el metodo HTTP.  
+*/
+
+app.get('/mostrarInfo',(req,res)=>{
+    console.log(req);
+    console.log("Procesando solicitud");
+    //responder. 
+    res.status(200).send(
+        {
+            mensaje: "Hola desde el backend!"
+        }
+    );
+    }
+);
+
